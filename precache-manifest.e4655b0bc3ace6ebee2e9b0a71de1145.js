ea55ec71af47c3f52f820bb480bba1320a4beec3
gr8fuldwg <gr8fuldwg@gmail.com>
2019-05-13 19:38:05 -0400

    Updates

---
+++ precache-manifest.e4655b0bc3ace6ebee2e9b0a71de1145.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984d9bebac82cbe22b850eb414e8a6e0",
+    "url": "/ProPortfolio/index.html"
+  },
+  {
+    "revision": "49a2179d9d767056b72a",
+    "url": "/ProPortfolio/static/css/main.f3d44817.chunk.css"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/ProPortfolio/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "49a2179d9d767056b72a",
+    "url": "/ProPortfolio/static/js/main.5458c70d.chunk.js"
+  },
+  {
+    "revision": "e79610f39ecd944e8303",
+    "url": "/ProPortfolio/static/js/runtime~main.8522265c.js"
+  },
+  {
+    "revision": "0b8242c24295869becc1836e93ec59a5",
+    "url": "/ProPortfolio/static/media/ChuckJokesPic.0b8242c2.png"
+  },
+  {
+    "revision": "92ebed13caad1d7e7b7268ee9ed1920e",
+    "url": "/ProPortfolio/static/media/WeatherAppPic1.92ebed13.PNG"
+  },
+  {
+    "revision": "dc0922689430599e555e399a051cb216",
+    "url": "/ProPortfolio/static/media/WeatherAppPic2.dc092268.PNG"
+  },
+  {
+    "revision": "4579f3a0eeac7793cb2373d33aff14a0",
+    "url": "/ProPortfolio/static/media/WeatherAppPic3.4579f3a0.PNG"
+  },
+  {
+    "revision": "c3cad2fe3a8d24dbee0e328ed58f551b",
+    "url": "/ProPortfolio/static/media/bgpic3.c3cad2fe.jpg"
+  },
+  {
+    "revision": "bef81c23c839571e3bfd479bb5558f1b",
+    "url": "/ProPortfolio/static/media/headshot.bef81c23.jpg"
+  }
+]);
