717ae9ba6deb838f0321eb10c3e3e32b7f44eaf6
gr8fuldwg <gr8fuldwg@gmail.com>
2020-03-09 22:41:09 -0400

    Updates

--- precache-manifest.9a93645d9445b6379be27eccafa6c433.js
+++ precache-manifest.9a93645d9445b6379be27eccafa6c433.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "abbbacaf43dea4cdbad42bada5275ea9",
+    "revision": "ea9a80c519e1a985c033a7c43e9fb410",
     "url": "/ProPortfolio/index.html"
   },
   {
-    "revision": "493c3d50b2b870832b45",
+    "revision": "ce9e07d1c5f5018ff451",
     "url": "/ProPortfolio/static/css/main.dba154f1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ProPortfolio/static/js/2.38f79e8a.chunk.js.LICENSE"
   },
   {
-    "revision": "493c3d50b2b870832b45",
-    "url": "/ProPortfolio/static/js/main.fec3d18b.chunk.js"
+    "revision": "ce9e07d1c5f5018ff451",
+    "url": "/ProPortfolio/static/js/main.2666d10e.chunk.js"
   },
   {
     "revision": "3ffea18c697e84aee527",
